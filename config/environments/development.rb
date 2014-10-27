4a6a8cbfd6132a49e2b74d21ec8a6a9b2a7b1799
Ola <oladele@gmail.com>
2014-10-27 06:29:24 -0400

    rails g devise:install and manual configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,10 @@
 Rails.application.configure do
+
+  #added manually per rails g install:devise instructions:
+  # #Ensure you have defined default url options in your environments files. Hereis an example of default_url_options appropriate for a development environment
+  # in config/environments/development.rb:
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
