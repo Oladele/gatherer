52ecafb94fa420e78b404645b0b071bdb7432a3a
Ola <oladele@gmail.com>
2014-10-27 06:50:47 -0400

    devise migration and passing auth feature spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,4 +47,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  #added manually for devise per rails test prescriptions
+  config.include Devise::TestHelpers, type: :controller
 end
